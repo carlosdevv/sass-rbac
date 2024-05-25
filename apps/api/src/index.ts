66d9baabe606d1363f9eb3bd53d0768a54255987
Carlos Lopes <carloslopessf@gmail.com>
2024-05-24 22:21:04 -0300

    feat: readme and setup backend

--- apps/api/src/index.ts
+++
@@ -1,6 +0,0 @@
-import { defineAbilitiesFor } from '@sass/auth'
-
-const ability = defineAbilitiesFor({ role: 'MEMBER', id: 'id' })
-const userCanInviteSomeoneElse = ability.can('delete', 'User')
-
-console.log('userCanInviteSomeoneElse?', userCanInviteSomeoneElse)
