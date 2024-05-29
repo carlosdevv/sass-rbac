8709ca870b598b691cbc9224dd53432bb29a69e6
carlosdevv <carloslopessf@gmail.com>
2024-05-28 23:20:34 -0300

    feat: recover and reset password

---
+++ apps/api/src/http/routes/auth/request-password-recover.ts
@@ -0,0 +1,47 @@
+import type { FastifyInstance } from 'fastify'
+import type { ZodTypeProvider } from 'fastify-type-provider-zod'
+import { z } from 'zod'
+
+import { prisma } from '@/lib/prisma'
+
+export async function requestPasswordRecover(app: FastifyInstance) {
+  app.withTypeProvider<ZodTypeProvider>().post(
+    '/password/recover',
+    {
+      schema: {
+        tags: ['Auth'],
+        summary: 'Request recover password',
+        body: z.object({
+          email: z.string().email(),
+        }),
+        response: {
+          201: z.null(),
+        },
+      },
+    },
+    async (request, reply) => {
+      const { email } = request.body
+
+      const userFromEmail = await prisma.user.findUnique({
+        where: {
+          email,
+        },
+      })
+
+      if (!userFromEmail) return reply.status(201).send() // We don't want to leak information about the user's existence
+
+      const { id: code } = await prisma.token.create({
+        data: {
+          type: 'PASSWORD_RECOVER',
+          userId: userFromEmail.id,
+        },
+      })
+
+      // Send email with recover link
+
+      console.log('Recover password code:', code)
+
+      return reply.status(201).send()
+    },
+  )
+}
