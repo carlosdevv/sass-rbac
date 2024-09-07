9540acbd8609367e1ffa5dfae5043c904e379548
carlosdevv <carloslopessf@gmail.com>
2024-09-07 20:52:20 -0300

    feat: create project sheet

--- apps/web/src/providers/app-provider.tsx
+++ apps/web/src/providers/app-provider.tsx
@@ -4,9 +4,9 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ThemeProvider } from 'next-themes'
 import { ReactNode } from 'react'
 
-export function Providers({ children }: { children: ReactNode }) {
-  const queryClient = new QueryClient()
+export const queryClient = new QueryClient()
 
+export function Providers({ children }: { children: ReactNode }) {
   return (
     <QueryClientProvider client={queryClient}>
       <ThemeProvider
