eef813b1a9cb20f02a8c1dab2f113d7a383f94db
carlosdevv <carloslopessf@gmail.com>
2024-09-07 21:31:20 -0300

    feat: update org

--- apps/web/src/app/(app)/create-organization/page.tsx
+++ apps/web/src/app/(app)/create-organization/page.tsx
@@ -1,6 +1,6 @@
 import { Header } from '@/components/header'
 
-import { OrganizationForm } from './components/organization-form'
+import { OrganizationForm } from '../org/organization-form'
 
 export default function CreateOrganization() {
   return (
